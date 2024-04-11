be728264ac3a07feccada0575d526c1809e313ec
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2024-04-11 17:19:23 +0200

    Fix some tests

--- src/app/components/modal-update-hero/modal-update-hero.component.spec.ts
+++ src/app/components/modal-update-hero/modal-update-hero.component.spec.ts
@@ -1,23 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ModalUpdateHeroComponent } from './modal-update-hero.component';
+import { ModalUpdateHeroComponent } from './modal-update-hero.component'
+import { HttpClientModule } from '@angular/common/http'
+import { HeroesService } from '../../services/heroes.service'
+import { HeroFormComponent } from '../hero-form/hero-form.component'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 
 describe('ModalUpdateHeroComponent', () => {
-  let component: ModalUpdateHeroComponent;
-  let fixture: ComponentFixture<ModalUpdateHeroComponent>;
+  let component: ModalUpdateHeroComponent
+  let fixture: ComponentFixture<ModalUpdateHeroComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ModalUpdateHeroComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ModalUpdateHeroComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      imports: [HeroFormComponent, HttpClientModule, NoopAnimationsModule],
+      providers: [
+        HeroesService,
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+      ],
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(ModalUpdateHeroComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
