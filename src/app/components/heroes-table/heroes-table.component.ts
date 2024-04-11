aedaee1aa578c723d1264e7fd77aba0f06704df1
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2024-04-11 17:03:34 +0200

    Update hero table to use new components

--- src/app/components/heroes-table/heroes-table.component.ts
+++ src/app/components/heroes-table/heroes-table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core'
+import { AfterViewInit, Component, Input, ViewChild, inject } from '@angular/core'
 import { MatSort, MatSortModule } from '@angular/material/sort'
 import { MatTableDataSource, MatTableModule } from '@angular/material/table'
 import { Hero } from '../../types/hero'
@@ -10,6 +10,10 @@ import { MatDialog } from '@angular/material/dialog'
 import { ModalHeroInfoComponent } from '../modal-hero-info/modal-hero-info.component'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { ModalNewHeroComponent } from '../modal-new-hero/modal-new-hero.component'
+import { HeroesService } from '../../services/heroes.service'
+import { HttpClientModule } from '@angular/common/http'
+import { ModalUpdateHeroComponent } from '../modal-update-hero/modal-update-hero.component'
 
 @Component({
   selector: 'heroes-table',
@@ -22,8 +26,10 @@ import { MatIconModule } from '@angular/material/icon'
     MatPaginatorModule,
     HeroesChipFilterComponent,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    HttpClientModule
   ],
+  providers: [HeroesService],
   templateUrl: './heroes-table.component.html',
   styleUrl: './heroes-table.component.css',
 })
@@ -43,8 +49,10 @@ export class HeroesTableComponent implements AfterViewInit {
     'occupationLabel',
     'memberOfLabel',
     'creatorLabel',
+    'actions'
   ]
   dataSource = new MatTableDataSource<Hero>([])
+  heroesService = inject(HeroesService)
   constructor(public dialog: MatDialog) {}
 
   ngAfterViewInit() {
@@ -53,13 +61,51 @@ export class HeroesTableComponent implements AfterViewInit {
     this.dataSource.filterPredicate = this.filterHeroesByNames
   }
 
-
   openHeroInformation(row: Hero): void {
     this.dialog.open(ModalHeroInfoComponent, {
       data: row,   
     })
   }
+
   openCreateHeroDialog(): void {
+    const dialogRef = this.dialog.open(ModalNewHeroComponent)
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.dataSource.data = [result, ...this.dataSource.data]
+      }
+    })
+  }
+
+  deleteHero(row: Hero): void {
+    this.heroesService.deleteHeroByName(row.nameLabel).subscribe(result => {
+      if (result) {
+        this.dataSource.data = this.dataSource.data.filter(
+          hero => hero.nameLabel !== row.nameLabel
+        )
+      }
+    })
+  }
+
+  updateHero(row: Hero): void {
+    const dialogRef = this.dialog.open(ModalUpdateHeroComponent, {
+      data: row,
+    })
+
+    dialogRef.afterClosed().subscribe(([updatedHero, prevHero]) => {
+      if (updatedHero) {
+
+        const prevData = this.dataSource.data
+
+        const index = prevData.findIndex(
+          hero => hero.nameLabel === prevHero.nameLabel
+        )
+        prevData[index] = updatedHero
+
+        this.dataSource.data = prevData
+      }
+    })
+
   }
 
   filterHeroes(heroes: string[]) {
